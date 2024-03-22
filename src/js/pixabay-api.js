13ea4abdc816ccfb37e00a113821514ef2b77282
IvyMtsh <147334689+IvyMtsh@users.noreply.github.com>
2024-03-22 23:25:03 +0200

    summary js

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,9 @@
+const API_KEY = '43020496-c204d90cd20fd14526ca38b9d';
+const baseURL = 'https://pixabay.com/api/';
+
+export async function fetchImages(searchQuery) {
+  const url = `${baseURL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.hits;
+}
